eef57bb559ac8aa9abf7df6dcb8d590001677d04
muhammadridho-debug <idomuhammad11@gmail.com>
2026-02-23 12:06:37 +0700

    menambahkan main code (input dan output), menginialisasikan variabel dan perbaikan pada variabel

--- app2.cpp
+++ app2.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 int main () {
     //mendeklarasikan variabel
-    int penyebut, pembilang, hasil;
+    float penyebut, pembilang, hasil;
 
+    // menampilkan output
+    cout << "Masukkan Pembilang :" << endl;
+    //memasukkan input
+    cin >> pembilang;
+
+    cout << "Masukkan Penyebut" << endl;
+    cin >> penyebut;
+
+    hasil = pembilang / penyebut;
+    cout << "Hasilnya :" << hasil;
 }
